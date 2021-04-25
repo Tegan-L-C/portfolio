d2d723d574eeccdf0fe08e642b38fbf9502c393a
Tegan-L-C <65487453+Tegan-L-C@users.noreply.github.com>
2021-04-25 22:03:12 +1000

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,12 @@
+//creates an alert
+alert("Welcome to my site!")
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function openMenu() {
+    var x = document.getElementById("portfolioTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+
+  }
